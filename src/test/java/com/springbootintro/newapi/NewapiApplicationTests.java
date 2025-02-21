950ccba43952197110d283f7c8c23d11bc9621b7
Miracle0312 <prajapatidhani06@gmail.com>
2025-02-22 02:38:14 +0530

    [Dhani] ADD. Use case 1- GET method

---
+++ src/test/java/com/springbootintro/newapi/NewapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootintro.newapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
