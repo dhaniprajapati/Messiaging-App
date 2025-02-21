1db401ad63ebf32f24c07cfbd26d7e00b2274762
Miracle0312 <prajapatidhani06@gmail.com>
2025-02-22 02:53:34 +0530

    [Dhani] ADD. Use case 2- GET method passing name as query parameter

--- src/main/java/com/springbootintro/newapi/controller/NewController.java
+++ src/main/java/com/springbootintro/newapi/controller/NewController.java
@@ -2,6 +2,7 @@ package com.springbootintro.newapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class NewController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    //UC2- use GET request method & pass name as query
+    //URL- http://localhost:8080/query?name=Dhani
+    @GetMapping("/query")
+    public String sayHelloName(@RequestParam String name){
+        return "Hello "+name+" from BridgeLabz";
     }
 }
